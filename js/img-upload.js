8c264ead6c65d71f33e2f334fdb3081fd283fd9f
fzred <1179750402@qq.com>
2016-07-12 23:13:22 +0800

    gif

--- js/img-upload.js
+++ js/img-upload.js
@@ -1,28 +1,33 @@
 class ImgUpload {
     constructor() {
+        const setFile = this.setFile
         document.addEventListener('paste', e => {
             const file = Array.from(e.clipboardData.items).find(item => item.type.indexOf('image') !== -1)
             if (!file) return
 
-            this.setFile(file.getAsFile())
+            setFile(file.getAsFile())
         })
         document.querySelector('#inputFile').addEventListener('change', e => {
-            this.setFile(e.target.files[0])
+            setFile(e.target.files[0])
         })
 
         document.addEventListener("dragover", e => e.preventDefault())
         document.addEventListener('drop', e => {
             e.preventDefault()
-            this.setFile(e.dataTransfer.files[0])
+            const file = e.dataTransfer.files[0]
+            if (file) setFile(file)
         })
     }
 
-    setFile(file) {
+    setFile(file, isGif = false) {
+        if (file.name) {
+            isGif = file.name.toLowerCase().indexOf('gif') !== -1
+        }
         const blobUrl = window.URL.createObjectURL(file)
         const img = new Image()
         img.src = blobUrl
         img.onload = () => {
-            imageDispose.setImg(img)
+            imageDispose.setImg({ file, img, isGif })
         }
     }
 }
