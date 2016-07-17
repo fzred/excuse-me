3d675fcc374a3f062daf54ac314d7981e2650651
fzred <1179750402@qq.com>
2016-07-17 19:15:17 +0800

    优化代码

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
-const imageDispose = new ImageDispose()
-const elResultImg = document.querySelector('#resultImg')
-new ImgUpload(({ file, img, isGif }) => {
-    imageDispose.generateImgSrc({ file, img, isGif }).then(src => {
-        elResultImg.src = src
+(()=> {
+    const imageDispose = new ImageDispose()
+
+    const elResultImg = document.querySelector('#resultImg')
+
+    function generateImgSrc({ file, img, isGif }) {
+        return new Promise(resolve => {
+            if (isGif) {
+                gifDecode(file).then(canvasData => {
+                    var gif = new GIF({
+                        workers: 2,
+                        workerScript: 'js/lib/gif.worker.js',
+                        quality: 10
+                    })
+                    canvasData.forEach(item => {
+                        new ImageDispose(item.canvas).draw()
+                        gif.addFrame(item.canvas, {
+                            delay: item.delay,
+                            copy: true
+                        })
+                    })
+                    gif.on('finished', function (blob) {
+                        resolve(window.URL.createObjectURL(blob))
+                    })
+                    gif.render()
+                })
+            } else {
+                imageDispose.displayImage(img)
+                resolve(imageDispose.getDataURL())
+            }
+        })
+    }
+
+    new ImgUpload(({ file, img, isGif }) => {
+        generateImgSrc({ file, img, isGif }).then(src => {
+            elResultImg.src = src
+        })
     })
-})
+})()
